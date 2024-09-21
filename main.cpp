986c99254b1faacfb9a92367e3ecec0df9ff28da
mihateren <mihateren2003@gmail.com>
2024-09-21 14:13:16 +0000

    add lr2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,111 @@
 #include <iostream>
-#include "include/countOnesInRange.h"
+#include "./include/Six.h"
 
 int main() {
-    int start, end;
-    std::cout << "enter the beginning of the sequence: ";
-    std::cin  >> start;
-    std::cout << "enter the ending of the sequence: ";
-    std::cin  >> end;
+    size_t lengthNumber;
+    std::cout << "Введите длину первого числа: ";
+    std::cin >> lengthNumber;
 
-    int totalOnes = 0;
-    
+    unsigned char defaultValue;
+    std::cout << "Введите значение по умолчанию для первого числа (0-5): ";
+    std::cin >> defaultValue;
+
+    Six number1; 
+
     try {
-        totalOnes = countOnesInRange(start, end);
-        std::cout << "result: " << totalOnes << std::endl;
-    } catch (const NegativeValueException& e) {
+        number1 = Six(lengthNumber, defaultValue);
+        std::cout << "Первое число: ";
+        for (size_t i = 0; i < number1.getSize(); ++i) {
+            std::cout << number1.getData()[i];
+        }
+        std::cout << std::endl;
+    } catch (const InvalidDigitException& e) {
         std::cerr << e.what() << std::endl;
-    } catch (const InvalidRangeException& e) {
+        return 1;
+    }
+
+    std::cout << "Введите 3 цифры для второго числа (от 0 до 5): ";
+    unsigned char arrayNumbers[3];
+    for (int i = 0; i < 3; ++i) {
+        std::cin >> arrayNumbers[i];
+    }
+
+    Six number2;
+    try {
+        number2 = Six({arrayNumbers[0], arrayNumbers[1], arrayNumbers[2]});
+        std::cout << "Второе число: ";
+        for (size_t i = 0; i < number2.getSize(); ++i) {
+            std::cout << number2.getData()[i];
+        }
+        std::cout << std::endl;
+    } catch (const InvalidDigitException& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    std::string stringNumber;
+    std::cout << "Введите строку из цифр для третьего числа (от 0 до 5): ";
+    std::cin >> stringNumber;
+
+    Six number3;
+    try {
+        number3 = Six(stringNumber);
+        std::cout << "Третье число: ";
+        for (size_t i = 0; i < number3.getSize(); ++i) {
+            std::cout << number3.getData()[i];
+        }
+        std::cout << std::endl;
+    } catch (const InvalidDigitException& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    Six sumResult = number1;
+    sumResult += number2;
+    std::cout << "Сумма Число 1 + Число 2: ";
+    for (size_t i = 0; i < sumResult.getSize(); ++i) {
+        std::cout << sumResult.getData()[i];
+    }
+    std::cout << std::endl;
+
+    Six subResult = number3;
+    try {
+        subResult -= number2;
+        std::cout << "Разность Число 3 - Число 2: ";
+        for (size_t i = 0; i < subResult.getSize(); ++i) {
+            std::cout << subResult.getData()[i];
+        }
+        std::cout << std::endl;
+    } catch (const SubtractionUnderflowException& e) {
         std::cerr << e.what() << std::endl;
     }
+
+    Six mulResult = number1;
+    mulResult *= number2;
+    std::cout << "Произведение Число 1 * Число 2: ";
+    for (size_t i = 0; i < mulResult.getSize(); ++i) {
+        std::cout << mulResult.getData()[i];
+    }
+    std::cout << std::endl;
+
+    Six copyOfNumber1 = number1;
+    std::cout << "Копия Числа 1: ";
+    for (size_t i = 0; i < copyOfNumber1.getSize(); ++i) {
+        std::cout << copyOfNumber1.getData()[i];
+    }
+    std::cout << std::endl;
+
+    Six movedNumber = std::move(number2);
+    std::cout << "Перемещенное Число 2: ";
+    for (size_t i = 0; i < movedNumber.getSize(); ++i) {
+        std::cout << movedNumber.getData()[i];
+    }
+    std::cout << std::endl;
+
+    std::cout << "Сравнение: " << std::endl;
+    std::cout << "Число 1 == Копия Числа 1: " << (number1 == copyOfNumber1) << std::endl;
+    std::cout << "Число 1 < Число 3: " << (number1 < number3) << std::endl;
+    std::cout << "Число 3 > Число 1: " << (number3 > number1) << std::endl;
 
     return 0;
 }
--- test/lrTests.cpp
+++ test/lrTests.cpp
@@ -1,57 +1,216 @@
 #include <gtest/gtest.h>
-#include "../include/countOnesInRange.h"
+#include "../include/Six.h"
 
-TEST(CountOnesInRangeTest, ReturnsCorrectResultForRange2To7)
-{
-    int start = 2;
-    int end = 7;
-    int result = countOnesInRange(start, end);
-    ASSERT_TRUE(result == 11);
+TEST(SixTest, DefaultConstructor) {
+    Six number;
+    size_t expectedSize = 1;
+    unsigned char expectedData[] = {'0'};
+
+    size_t realSize = number.getSize();
+    unsigned char* realData = number.getData();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realData[0] == expectedData[0]);
 }
 
-TEST(CountOnesInRangeTest, ReturnsCorrectResultForRange0To1)
-{
-    int start = 0;
-    int end = 1;
-    int result = countOnesInRange(start, end);
-    ASSERT_TRUE(result == 1);
+TEST(SixTest, ConstructorWithDefaultValue) {
+    size_t length = 2;
+    unsigned char defaultValue = '3';
+    size_t expectedSize = 2;
+    unsigned char expectedData[] = {'3', '3'};
+
+    Six number(length, defaultValue);
+    size_t realSize = number.getSize();
+    unsigned char* realData = number.getData();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realData[0] == expectedData[0]);
+    ASSERT_TRUE(realData[1] == expectedData[1]);
 }
 
-TEST(CountOnesInRangeTest, ReturnsCorrectResultForRange0To0)
-{
-    int start = 0;
-    int end = 0;
-    int result = countOnesInRange(start, end);
-    ASSERT_TRUE(result == 0);
+TEST(SixTest, ConstructorWithInitializerList) {
+    std::initializer_list<unsigned char> digits = {'1', '2', '3'};
+    size_t expectedSize = 3;
+    unsigned char expectedData[] = {'1', '2', '3'};
+
+    Six number(digits);
+    size_t realSize = number.getSize();
+    unsigned char* realData = number.getData();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realData[0] == expectedData[0]);
+    ASSERT_TRUE(realData[1] == expectedData[1]);
 }
 
-TEST(CountOnesInRangeTest, ReturnsCorrectResultForRange1To1)
-{
-    int start = 1;
-    int end = 1;
-    int result = countOnesInRange(start, end);
-    ASSERT_TRUE(result == 1);
+TEST(SixTest, ConstructorWithString) {
+    std::string numberString = "123";
+    size_t expectedSize = 3;
+    unsigned char expectedData[] = {'1', '2', '3'};
+
+    Six number(numberString);
+    size_t realSize = number.getSize();
+    unsigned char* realData = number.getData();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realData[0] == expectedData[0]);
+    ASSERT_TRUE(realData[1] == expectedData[1]);
+    ASSERT_TRUE(realData[2] == expectedData[2]);
 }
 
-TEST(CountOnesInRangeTest, ThrowsNegativeValueExceptionForNegativeStart)
-{
-    int start = -1;
-    int end = 7;
-    ASSERT_THROW(countOnesInRange(start, end), NegativeValueException);
+TEST(SixTest, CopyConstructor) {
+    Six original({'1', '2', '3'});
+    size_t expectedSize = 3;
+    unsigned char expectedData[] = {'1', '2', '3'};
+
+    Six copy(original);
+    size_t realSize = copy.getSize();
+    unsigned char* realData = copy.getData();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realData[0] == expectedData[0]);
+    ASSERT_TRUE(realData[1] == expectedData[1]);
+    ASSERT_TRUE(realData[2] == expectedData[2]);
 }
 
-TEST(CountOnesInRangeTest, ThrowsNegativeValueExceptionForNegativeStartAndEnd)
-{
-    int start = -1;
-    int end = -10;
-    ASSERT_THROW(countOnesInRange(start, end), NegativeValueException);
+TEST(SixTest, MoveConstructor) {
+    Six original({'1', '2', '3'});
+    unsigned char expectedMovedData[] = {'1', '2', '3'};
+    size_t expectedMovedSize = 3;
+    unsigned char* expectedOriginalData = nullptr;
+    size_t expectedOriginalSize = 0;
+    
+    Six moved(std::move(original));
+    size_t movedSize = moved.getSize();
+    unsigned char* movedData = moved.getData();
+
+    ASSERT_TRUE(movedSize == expectedMovedSize);
+    ASSERT_TRUE(movedData[0] == expectedMovedData[0]);
+    ASSERT_TRUE(movedData[1] == expectedMovedData[1]);
+    ASSERT_TRUE(movedData[2] == expectedMovedData[2]);
+    ASSERT_TRUE(original.getSize() == expectedOriginalSize);
+    ASSERT_TRUE(original.getData() == expectedOriginalData);
 }
 
-TEST(CountOnesInRangeTest, ThrowsInvalidRangeExceptionForEndingGreaterThanBeginning)
-{
-    int start = 5;
-    int end = 1;
-    ASSERT_THROW(countOnesInRange(start, end), InvalidRangeException);
+
+TEST(SixTest, AssignmentOperator) {
+    Six original({'1', '2', '3'});
+    Six assigned({'4', '5'});
+    size_t expectedSize = 3;
+    unsigned char expectedData[] = {'1', '2', '3'};
+    
+    assigned = original;
+    unsigned char* realResult = assigned.getData();
+    size_t realSize = assigned.getSize();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realResult[0] == expectedData[0]);
+    ASSERT_TRUE(realResult[1] == expectedData[1]);
+    ASSERT_TRUE(realResult[2] == expectedData[2]);
+}
+
+
+TEST(SixTest, AdditionOperator) {
+    Six number1({'1', '2', '3'});
+    Six number2({'2', '2', '2'});
+    unsigned char expectedResult[] = {'3', '4', '5'};
+    size_t expectedSize = 3;
+
+    number1 += number2;
+    unsigned char* realResult = number1.getData();
+    size_t realSize = number1.getSize();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realResult[0] == expectedResult[0]);
+    ASSERT_TRUE(realResult[1] == expectedResult[1]);
+    ASSERT_TRUE(realResult[2] == expectedResult[2]);
+}
+
+
+TEST(SixTest, SubtractionOperator) {
+    Six number1({'5', '4', '3'});
+    Six number2({'2', '2', '2'});
+    unsigned char expectedResult[] = {'3', '2', '1'};
+    size_t expectedSize = 3;
+
+    number1 -= number2;
+    unsigned char* realResult = number1.getData();
+    size_t realSize = number1.getSize();
+
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realResult[0] == expectedResult[0]);
+    ASSERT_TRUE(realResult[1] == expectedResult[1]);
+    ASSERT_TRUE(realResult[2] == expectedResult[2]);
+}
+
+
+TEST(SixTest, MultiplicationOperator) {
+    Six number1({'2', '1'});
+    Six number2({'2', '2'});
+    unsigned char expectedResult[] = {'4', '0', '3'};
+    size_t expectedSize = 3;
+    
+    number1 *= number2;
+    unsigned char* realResult = number1.getData();
+    size_t realSize = number1.getSize();
+    
+    ASSERT_TRUE(realSize == expectedSize);
+    ASSERT_TRUE(realResult[0] == expectedResult[0]);
+    ASSERT_TRUE(realResult[1] == expectedResult[1]);
+    ASSERT_TRUE(realResult[2] == expectedResult[2]);
+}
+
+
+TEST(SixTest, ComparisonOperators) {
+    Six number1({'1', '2', '3'});
+    Six number2({'1', '2', '3'});
+    Six number3({'2', '2', '3'});
+
+    bool is1Equal2 = (number1 == number2);
+    bool is1LessThan2 = (number1 < number2);
+    bool is1GreaterThan2 = (number1 > number2);
+    bool is1LessThanNumber3 = (number1 < number3);
+    bool is1GreaterThanNumber3 = (number1 > number3);
+
+    ASSERT_TRUE(is1Equal2);
+    ASSERT_FALSE(is1LessThan2);
+    ASSERT_FALSE(is1GreaterThan2);
+    ASSERT_TRUE(is1LessThanNumber3);
+    ASSERT_FALSE(is1GreaterThanNumber3);
+}
+
+
+TEST(SixTest, InvalidDigitExceptionTest) {
+    std::initializer_list<unsigned char> invalidDigits1 = { '6', '1' };
+    std::initializer_list<unsigned char> invalidDigits2 = { 'a', '1' };
+
+    EXPECT_THROW(Six number1(invalidDigits1), InvalidDigitException);
+    EXPECT_THROW(Six number2(invalidDigits2), InvalidDigitException);
+}
+
+TEST(SixTest, SelfAssignmentExceptionTest) {
+    Six number({'1', '2', '3'});
+
+    EXPECT_THROW(number = number, SelfAssignmentException);
+}
+
+TEST(SixTest, SizeMismatchExceptionTest) {
+    Six small({'1', '2'});
+    Six large({'5', '5', '5'});
+
+    EXPECT_THROW(small -= large, SizeMismatchException);
+}
+
+TEST(SixTest, SubtractionUnderflowExceptionTest) {
+    Six number1({'1', '1', '1'});
+    Six number2({'5', '5', '5'});
+
+    EXPECT_THROW(number1 -= number2, SubtractionUnderflowException);
+}
+
+TEST(SixTest, MemoryAllocationExceptionTest) {
+    size_t bigSize = SIZE_MAX;
+
+    EXPECT_THROW(Six number(bigSize, '1'), MemoryAllocationException);
 }
 
 int main(int argc, char **argv) {
