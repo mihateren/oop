c4ac956fd45fee5498b133c5ef6286183cbe85c1
mihateren <mihateren2003@gmail.com>
2024-11-24 13:51:10 +0000

    add filelog

--- cmake-build-release/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/mai/2/oop/cmake-build-release/_deps/googletest-src
-# Build directory: D:/mai/2/oop/cmake-build-release/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
